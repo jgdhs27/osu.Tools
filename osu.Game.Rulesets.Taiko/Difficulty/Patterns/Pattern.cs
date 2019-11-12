78b4a88c6dcb79dc2132e2b7b36c902eaeb4bbcc
jgdhs27 <markusobendrauf@gmail.com>
2019-11-12 23:31:55 +0100

    Added Pattern classes

---
+++ osu.Game.Rulesets.Taiko/Difficulty/Patterns/Pattern.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Rulesets.Taiko.Difficulty.Patterns
+{
+    public abstract class Pattern
+    {
+        protected readonly int Length;
+        public abstract override int GetHashCode();
+        public abstract override bool Equals(object obj);
+
+        protected Pattern(int length)
+        {
+            this.Length = length;
+        }
+
+    }
+}
---
+++ osu.Game.Rulesets.Taiko/Difficulty/Patterns/RhythmPattern.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Rulesets.Taiko.Difficulty.Preprocessing;
+
+namespace osu.Game.Rulesets.Taiko.Difficulty.Patterns
+{
+    public class RhythmPattern : Pattern
+    {
+
+        private readonly int[] rhythmIDs;
+
+        public bool Equals(int[] x, int[] y)
+        {
+            if (x.Length != y.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] != y[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            for (int i = 0; i < rhythmIDs.Length; i++)
+            {
+                unchecked // ignore int overflows
+                {
+                    hash = hash * 23 + rhythmIDs[i];
+                }
+            }
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != this.GetType()) return false;
+
+            RhythmPattern r = (RhythmPattern)obj;
+            if (r.Length != this.Length) return false;
+
+            for (int i = 0; i < this.Length; i++)
+            {
+                if (r.rhythmIDs[i] != this.rhythmIDs[i]) return false;
+            }
+
+            return true;
+
+        };
+
+        public RhythmPattern(IReadOnlyList<TaikoDifficultyHitObject> recentObjects, int length)
+            : base(length)
+        {
+            rhythmIDs = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                rhythmIDs[i] = recentObjects[recentObjects.Count - length + i].RhythmID;
+            }
+        }
+    }
+}
