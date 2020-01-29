153f207154981923dd5762dc753c3a540313d6e5
jgdhs27 <markusobendrauf@gmail.com>
2020-01-29 13:09:59 +0100

    2 Handed stamina skil;

---
+++ osu.Game.Rulesets.Taiko/Difficulty/Skills/Stamina.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Difficulty.Skills;
+
+namespace osu.Game.Rulesets.Taiko.Difficulty.Skills
+{
+    public class Stamina : Skill
+    {
+
+        private int hand;
+        private int noteNumber = 0;
+
+        protected override double SkillMultiplier => 1;
+        protected override double StrainDecayBase => 0.6;
+
+        protected override double StrainValueOf(DifficultyHitObject current)
+        {
+            noteNumber += 1;
+            noteNumber = noteNumber % 2;
+            if (noteNumber == hand)
+                return 1;
+            return 0;
+        }
+
+        public Stamina(bool rightHand)
+        {
+            hand = 0;
+            if (rightHand)
+            {
+                hand = 1;
+            }
+        }
+
+    }
+}
